51ca0a9ec8fda8263b02114c4f8f1cf3654bcdce
mihaicaragea <carageamihai@gmail.com>
2025-07-01 08:47:31 +0300

    First commit

---
+++ src/main/kotlin/com/financial/funds_transfer/FundsTransferApplication.kt
@@ -0,0 +1,11 @@
+package com.financial.funds_transfer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FundsTransferApplication
+
+fun main(args: Array<String>) {
+	runApplication<FundsTransferApplication>(*args)
+}
---
+++ src/test/kotlin/com/financial/funds_transfer/FundsTransferApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.financial.funds_transfer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FundsTransferApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
